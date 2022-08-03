d0f3333ace14132da6e9ab6a20d1a77160b894eb
ajarek <ajarek@poczta.onet.pl>
2022-08-04 01:03:57 +0200

    config CRA +eslint

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const TEXTS = ['Declarative', 'Component-Based', 'Learn Once, Write Anywhere']
+
+const firstElement = (
+    <div>
+        <h1>Hello React</h1>
+        <ul>
+          {TEXTS.map((text, index) =>
+             <li key={index}>{text}</li>
+          )}
+        </ul>
+    </div>
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(firstElement)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()

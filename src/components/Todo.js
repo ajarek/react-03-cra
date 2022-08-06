86378d3af879cf4936d40029ab3326691cbd2282
ajarek <ajarek@poczta.onet.pl>
2022-08-06 23:04:44 +0200

    add delete, responsive

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -21,15 +21,29 @@ class Todo extends React.Component {
     )
   }
 
+  deleteItem (id) {
+    const newElements = this.state.elements
+    newElements.forEach(element => {
+      if (element.id === id) {
+        newElements.splice(newElements.indexOf(element), 1)
+        this.setState({ elements: newElements })
+      }
+    })
+  }
+
   InputHandler (event) {
     this.setState({ inputValue: event.target.value })
   }
 
   addItem = () => {
-    const item = { id: this.state.elements.length + 1, text: this.state.inputValue, completed: false }
-    const newElements = [item, ...this.state.elements]
-    this.setState({ elements: newElements })
-    this.setState({ inputValue: '' })
+    if (this.state.inputValue !== '') {
+      const item = { id: this.state.elements.length + 1, text: this.state.inputValue, completed: false }
+      const newElements = [item, ...this.state.elements]
+      this.setState({ elements: newElements })
+      this.setState({ inputValue: '' })
+    } else {
+      alert('Wpisz coś')
+    }
   }
 
   render () {
@@ -39,11 +53,18 @@ class Todo extends React.Component {
         key={element.id}
       >
         <h4>{element.text}</h4>
-        <button
-          className={'btn'}
-          onClick={() => this.markClicked(element.id)}
-        >Zakończone
-        </button>
+        <div className={'row'}>
+          <button
+            className={'btn'}
+            onClick={() => this.markClicked(element.id)}
+          >Zakończone
+          </button>
+          <button
+            className={'btn'}
+            onClick={() => this.deleteItem(element.id)}
+          >❌
+          </button>
+        </div>
       </li>)
     return (
       <div className={'todo'}>

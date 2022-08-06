e61796dd4fe9d80e4bb6acdbb7f5576f50ada752
ajarek <ajarek@poczta.onet.pl>
2022-08-06 22:21:19 +0200

    add inputHandler ,addItem

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,9 @@ class Todo extends React.Component {
     elements: [
       { id: 1, text: 'Naucz się React', completed: false },
       { id: 2, text: 'Zrób pranie', completed: true }
-    ]
+    ],
+    inputValue: ''
+
   }
 
   markClicked (id) {
@@ -19,6 +21,17 @@ class Todo extends React.Component {
     )
   }
 
+  InputHandler (event) {
+    this.setState({ inputValue: event.target.value })
+  }
+
+  addItem = () => {
+    const item = { id: this.state.elements.length + 1, text: this.state.inputValue, completed: false }
+    const newElements = [item, ...this.state.elements]
+    this.setState({ elements: newElements })
+    this.setState({ inputValue: '' })
+  }
+
   render () {
     const elements = this.state.elements.map(element =>
       <li
@@ -26,11 +39,29 @@ class Todo extends React.Component {
         key={element.id}
       >
         <h4>{element.text}</h4>
-        <button onClick={() => this.markClicked(element.id)} >Zakończone</button>
+        <button
+          className={'btn'}
+          onClick={() => this.markClicked(element.id)}
+        >Zakończone
+        </button>
       </li>)
     return (
       <div className={'todo'}>
         <h1>Todo List</h1>
+        <div className={'wrapper'}>
+          <input
+            className={'input'}
+            type={'text'}
+            onChange={(event) => this.InputHandler(event)}
+            value={this.state.inputValue}
+          />
+
+          <button
+            className={'btn'}
+            onClick={this.addItem}
+          >Dodaj
+          </button>
+        </div>
         {elements}
 
       </div>
